9cb74348e953fbdba034e507f1234bced534d702
alisouran <alirezasoran@gmail.com>
2019-10-14 00:30:27 +0330

    res

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,21 @@
-$(document).ready(function() {});
+let previous = -1;
+$(".icon[data-index]").click(function() {
+  $(this).addClass("initialised");
+  let index = $(this).attr("data-index");
+  let navtab = $(this)
+    .closest("nav.tab")
+    .addClass("moving")
+    .attr("data-selected", index);
+  if (previous == -1)
+    navtab.find('.icon[data-index="2"]').addClass("initialised");
+  if ((previous == 1 && index == 3) || (previous == 3 && index == 1)) {
+    navtab.find('.icon[data-index="2"]').removeClass("initialised");
+    setTimeout(function() {
+      navtab.find('.icon[data-index="2"]').addClass("initialised");
+    });
+  }
+  previous = index;
+  setTimeout(function() {
+    navtab.removeClass("moving").removeClass("hidemiddle");
+  }, 750);
+});
